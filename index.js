5ff528b5652dd158fa914371b0c59e452b61a901
AntonioEsp <github email address>
2021-08-17 20:11:40 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+function myEach() {
+
+
+}
+
+function myMap() {
+    
+}
+
+function myReduce() {
+
+}
+
+function myFind(params) {
+    
+}
+
+function myFiller(params) {
+    
+}
+
+function mySize(params) {
+    
+}
+
+function myFirst(params) {
+    
+}
+
+function myLast(params) {
+    
+}
